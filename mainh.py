8b1b04a0dfb3bb6068e35c3bfb234b423a33437c
ali7haider <haider5568761@gmail.com>
2024-04-26 23:13:42 +0500

    Saving images of number plate of motorcyclist not wearing helmet

--- mainh.py
+++ mainh.py
@@ -3,6 +3,7 @@ import pandas as pd
 from ultralytics import YOLO
 import cvzone
 import numpy as np
+import time
 
 
 
@@ -12,15 +13,22 @@ def RGB(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE :  
         point = [x, y]
         print(point)
-  
+
+def inside_box(big_box, small_box):
+	x1 = small_box[0] - big_box[0]
+	y1 = small_box[1] - big_box[1]
+	x2 = big_box[2] - small_box[2]
+	y2 = big_box[3] - small_box[3]
+	return not bool(min([x1, y1, x2, y2, 0]))
+
         
 
 cv2.namedWindow('RGB')
 cv2.setMouseCallback('RGB', RGB)
-cap=cv2.VideoCapture('Part1.mp4')
+cap=cv2.VideoCapture('Part2.mp4')
 
 
-my_file = open("coco1.txt", "r")
+my_file = open("classes.txt", "r")
 data = my_file.read()
 class_list = data.split("\n") 
 #print(class_list)
@@ -38,6 +46,9 @@ while True:
     if count % 3 != 0:
         continue
     frame=cv2.resize(frame,(1020,500))
+    original_frame = frame.copy()
+
+
    
 
     results=model.predict(frame)
@@ -47,8 +58,10 @@ while True:
 #    print(px)
     
     list=[]
+    rider_list = []
+    helmet_list = []
+    number_list = []
     for index,row in px.iterrows():
-#        print(row)
  
         x1=int(row[0])
         y1=int(row[1])
@@ -56,11 +69,37 @@ while True:
         y2=int(row[3])
         d=int(row[5])
         c=class_list[d]
+        
+        if c == 'helmet':
+            helmet_list.append(row)
+        elif c == 'motorcyclist':
+            rider_list.append(row)
+        elif c == 'license_plate':
+            number_list.append(row)
  
       
         cv2.rectangle(frame,(x1,y1),(x2,y2),(255,0,255),2)
         cvzone.putTextRect(frame,f'{c}',(x1,y1),1,1)
+    for rider in rider_list:
+        x1r, y1r, x2r, y2r, _ ,_= rider
+        no_helmet = True
+        for helmet in helmet_list:
+            x1h, y1h, x2h, y2h, _ ,_= helmet
+            if inside_box([x1r, y1r, x2r, y2r], [x1h, y1h, x2h, y2h]):
+                no_helmet = False
+                break
 
+        if no_helmet:
+            for number in number_list:
+                x1n, y1n, x2n, y2n, _ ,_= number
+                if inside_box([x1r, y1r, x2r, y2r], [x1n, y1n, x2n, y2n]):
+                    plate_region = original_frame[int(y1n):int(y2n), int(x1n):int(x2n)]
+                    cv2.imwrite(f"license_plates/{time.time()}_plate.jpg", plate_region)
+
+
+    print("rider_list:",rider_list)
+    print("helmet_list:",helmet_list)
+    print("number_list:",number_list)
 
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1)&0xFF==27:
